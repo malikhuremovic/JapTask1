34f65cb1de351bf0c3f4be66589f5540350a4489
malikhuremovic <112878831+malikhuremovic@users.noreply.github.com>
2022-09-20 12:36:28 +0200

    Revert "Database connection migration"

--- Back-end/JAPManagementSystem/JAPManagementSystem/Program.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Program.cs
@@ -1,6 +1,3 @@
-using JAPManagementSystem.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
