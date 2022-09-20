9fac22c0ec61d94995c167d06290b5e80be28cc2
Malik Huremovic <malikhu@maestralsolutions.com>
2022-09-20 12:18:48 +0200

    Added models: Student, User, Selection, Program

---
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Models/JapProgram.cs
@@ -0,0 +1,9 @@
+﻿namespace JAPManagementSystem.Models
+{
+    public class JapProgram
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
