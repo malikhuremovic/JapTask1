6d97b29ecd1d89b549c7ff7381cf0feb35e3d448
Malik Huremovic <malikhu@maestralsolutions.com>
2022-09-22 16:04:22 +0200

    Code consistency

--- Back-end/JAPManagementSystem/JAPManagementSystem/DTOs/Student/GetStudentDto.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/DTOs/Student/GetStudentDto.cs
@@ -5,9 +5,9 @@ namespace JAPManagementSystem.DTOs.Student
 {
     public class GetStudentDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public StudentStatus Status { get; set; }
         public GetSelectionDto? Selection { get; set; }
     }
--- Back-end/JAPManagementSystem/JAPManagementSystem/Models/Student.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Models/Student.cs
@@ -3,11 +3,11 @@
     public class Student
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty; 
         public StudentStatus Status { get; set; } = StudentStatus.InProgram;
-        public int? SelectionId { get; set; }
-        public Selection Selection { get; set; }
+        public int? SelectionId { get; set; } = null;
+        public Selection? Selection { get; set; } = null;
     }
 }
