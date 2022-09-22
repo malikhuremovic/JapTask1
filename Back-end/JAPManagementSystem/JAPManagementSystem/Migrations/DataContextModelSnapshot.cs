2c40ec9902591149800b99fe19aa1331b1f09b5b
Malik Huremovic <malikhu@maestralsolutions.com>
2022-09-22 14:33:43 +0200

    Added migration for slightly updated Models. Added First name and Last name for student

--- Back-end/JAPManagementSystem/JAPManagementSystem/Migrations/DataContextModelSnapshot.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Migrations/DataContextModelSnapshot.cs
@@ -83,7 +83,11 @@ namespace JAPManagementSystem.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Name")
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("LastName")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
