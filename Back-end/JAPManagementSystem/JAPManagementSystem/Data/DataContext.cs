7311bcf6c5312f45e55256cc87b903d86e9cfe9b
Malik Huremovic <malikhu@maestralsolutions.com>
2022-09-27 09:16:36 +0200

    Seeded data into database

--- Back-end/JAPManagementSystem/JAPManagementSystem/Data/DataContext.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Data/DataContext.cs
@@ -12,6 +12,126 @@ namespace JAPManagementSystem.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder _modelBuilder)
+        {
+            _modelBuilder.Entity<JapProgram>().HasData(
+            new JapProgram
+            {
+                Id = 1,
+                Name = "JAP DEV",
+                Content = ".NET & React.js"
+            },
+            new JapProgram
+            {
+                Id = 2,
+                Name = "JAP QA",
+                Content = "Selenium & Unit & Integration Testing"
+            },
+            new JapProgram
+            {
+                Id = 3,
+                Name = "JAP DevOps",
+                Content = "Linux & Docker"
+            });
+
+            _modelBuilder.Entity<Selection>().HasData(
+                new Selection
+                {
+                    Id = 1,
+                    Name = "Dev Jap September",
+                    DateStart = new DateTime(2022, 9, 5),
+                    DateEnd = new DateTime(2022, 11, 5),
+                    JapProgramId = 1,
+                    Status = SelectionStatus.Active
+                },
+                new Selection
+                {
+                    Id = 2,                   
+                    Name = "Dev QA June",
+                    DateStart = new DateTime(2022, 6, 5),
+                    DateEnd = new DateTime(2022, 8, 5),
+                    JapProgramId = 2,
+                    Status = SelectionStatus.Completed
+                }
+             ) ;
+
+
+            _modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "johndoe@mail.com",
+                    Status = StudentStatus.InProgram,
+                    SelectionId = 1,
+                },
+                new Student
+                {
+                    Id = 2,
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Email = "janendoe@mail.com",
+                    Status = StudentStatus.Success,
+                    SelectionId = 1
+
+                }, new Student
+                {
+                    Id = 3,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "johndoe@mail.com",
+                    Status = StudentStatus.InProgram,
+                    SelectionId = 1,
+                }, new Student
+                {
+                    Id = 4,
+                    FirstName = "Malik",
+                    LastName = "Huremović",
+                    Email = "malikhuremovic01@mail.com",
+                    Status = StudentStatus.InProgram,
+                    SelectionId = 1,
+                }, 
+                new Student
+                {
+                    Id = 5,
+                    FirstName = "Ishak",
+                    LastName = "Isabegović",
+                    Email = "isakIsabegovic@mail.com",
+                    Status = StudentStatus.InProgram,
+                    SelectionId = 1,
+                },
+                new Student
+                {
+                    Id = 6,
+                    FirstName = "Emir",
+                    LastName = "Bajrić",
+                    Email = "emirbajric@mail.com",
+                    Status = StudentStatus.Success,
+                    SelectionId = 2,
+                },
+                new Student
+                {
+                    Id = 7,
+                    FirstName = "Zlatan",
+                    LastName = "Sprečo",
+                    Email = "zlatansprečo@mail.com",
+                    Status = StudentStatus.Success,
+                    SelectionId = 2,
+                },
+                new Student
+                {
+                    Id = 8,
+                    FirstName = "Sanel",
+                    LastName = "Hodžić",
+                    Email = "sanelh@mail.com",
+                    Status = StudentStatus.Success,
+                    SelectionId = 2,
+                }
+            );
+
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<JapProgram> JapPrograms { get; set; }
--- Back-end/JAPManagementSystem/JAPManagementSystem/Migrations/DataContextModelSnapshot.cs
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Migrations/DataContextModelSnapshot.cs
@@ -66,6 +66,26 @@ namespace JAPManagementSystem.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("JapPrograms");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Content = ".NET & React.js",
+                            Name = "JAP DEV"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Content = "Selenium & Unit & Integration Testing",
+                            Name = "JAP QA"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Content = "Linux & Docker",
+                            Name = "JAP DevOps"
+                        });
                 });
 
             modelBuilder.Entity("JAPManagementSystem.Models.Selection", b =>
@@ -97,6 +117,26 @@ namespace JAPManagementSystem.Migrations
                     b.HasIndex("JapProgramId");
 
                     b.ToTable("Selections");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            DateEnd = new DateTime(2022, 11, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            DateStart = new DateTime(2022, 9, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            JapProgramId = 1,
+                            Name = "Dev Jap September",
+                            Status = 1
+                        },
+                        new
+                        {
+                            Id = 2,
+                            DateEnd = new DateTime(2022, 8, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            DateStart = new DateTime(2022, 6, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            JapProgramId = 2,
+                            Name = "Dev QA June",
+                            Status = 2
+                        });
                 });
 
             modelBuilder.Entity("JAPManagementSystem.Models.Student", b =>
@@ -130,6 +170,80 @@ namespace JAPManagementSystem.Migrations
                     b.HasIndex("SelectionId");
 
                     b.ToTable("Students");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Email = "johndoe@mail.com",
+                            FirstName = "John",
+                            LastName = "Doe",
+                            SelectionId = 1,
+                            Status = 1
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Email = "janendoe@mail.com",
+                            FirstName = "Jane",
+                            LastName = "Doe",
+                            SelectionId = 1,
+                            Status = 2
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Email = "johndoe@mail.com",
+                            FirstName = "John",
+                            LastName = "Doe",
+                            SelectionId = 1,
+                            Status = 1
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Email = "malikhuremovic01@mail.com",
+                            FirstName = "Malik",
+                            LastName = "Huremović",
+                            SelectionId = 1,
+                            Status = 1
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Email = "isakIsabegovic@mail.com",
+                            FirstName = "Ishak",
+                            LastName = "Isabegović",
+                            SelectionId = 1,
+                            Status = 1
+                        },
+                        new
+                        {
+                            Id = 6,
+                            Email = "emirbajric@mail.com",
+                            FirstName = "Emir",
+                            LastName = "Bajrić",
+                            SelectionId = 2,
+                            Status = 2
+                        },
+                        new
+                        {
+                            Id = 7,
+                            Email = "zlatansprečo@mail.com",
+                            FirstName = "Zlatan",
+                            LastName = "Sprečo",
+                            SelectionId = 2,
+                            Status = 2
+                        },
+                        new
+                        {
+                            Id = 8,
+                            Email = "sanelh@mail.com",
+                            FirstName = "Sanel",
+                            LastName = "Hodžić",
+                            SelectionId = 2,
+                            Status = 2
+                        });
                 });
 
             modelBuilder.Entity("JAPManagementSystem.Models.User", b =>
