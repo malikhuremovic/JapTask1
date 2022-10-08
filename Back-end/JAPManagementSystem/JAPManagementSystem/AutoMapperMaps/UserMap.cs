1e247b02bc6ba8d1afb45d83e09d466b07241d53
Malik Huremovic <malikhu@maestralsolutions.com>
2022-10-08 16:47:27 +0200

    Added maps to support creating a new user with hashed password

---
+++ Back-end/JAPManagementSystem/JAPManagementSystem/AutoMapperMaps/UserMap.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using JAPManagementSystem.DTOs.User;
+using JAPManagementSystem.Models;
+
+namespace JAPManagementSystem.AutoMapperMaps
+{
+    public class UserMap : Profile
+    {
+        public UserMap()
+        {
+            CreateMap<StudentUserCreatedDto, Student>();
+            CreateMap<User, GetUserDto>();
+        }
+    }
+}
