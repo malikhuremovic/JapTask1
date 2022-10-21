ca85faf2811347010d023068620d1d159d3f8fa5
Malik Huremovic <malikhu@maestralsolutions.com>
2022-10-21 14:24:14 +0200

    Added item and programitem models to support many to many relationship between japprogram and item

---
+++ Back-end/JAPManagementSystem/JAPManagementSystem/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace JAPManagementSystem.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public int ExpectedHours { get; set; }
+        public bool isEvent { get; set; }
+        public List<JapProgram> Programs { get; set; } = new List<JapProgram>();
+        public List<ProgramItem> ProgramItem { get; set; } = new List<ProgramItem>();
+    }
+}
