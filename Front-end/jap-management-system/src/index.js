6e5dd94eedc1a00937204d022e82c1ebbe92e190
Malik Huremovic <malikhu@maestralsolutions.com>
2022-10-22 20:35:21 +0200

    Change fixed

--- Front-end/jap-management-system/src/index.js
+++ Front-end/jap-management-system/src/index.js
@@ -25,7 +25,7 @@ const initialize = async () => {
       alert('Authentication has failed. Invalid token. Please log in again.');
       logoutUser();
     }
-    alert('No service available');
+    alert('No service available. Please, try again later');
     logoutUser();
   }
   return user;
