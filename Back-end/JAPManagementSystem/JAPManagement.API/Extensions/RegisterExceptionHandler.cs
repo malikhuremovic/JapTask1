10bfa6c6e4938d0b0a508ca5cccc5fabc4c75e39
Malik Huremovic <malikhu@maestralsolutions.com>
2022-10-28 11:12:42 +0200

    Register global exception handler

---
+++ Back-end/JAPManagementSystem/JAPManagement.API/Extensions/RegisterExceptionHandler.cs
@@ -0,0 +1,47 @@
+﻿using JAPManagement.ExceptionHandler;
+using JAPManagement.ExceptionHandler.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using NLog.Fluent;
+using System.Text;
+
+namespace JAPManagement.API.Extensions
+{
+    public static class RegisterExceptionHandler
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    StringBuilder message = new StringBuilder();
+                    if(contextFeature?.Error is Exception)
+                        {
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        message.Append("Internal Server Error. Please try again later.");
+                    }
+                    else if(contextFeature?.Error is BadRequestException)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                        context.Response.ContentType = "application/json";
+                        message.Append("Bad Request. Parameters not valid.");
+                    }
+                    else if (contextFeature?.Error is EntityNotFoundException)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                        context.Response.ContentType = "application/json";
+                        message.Append("The requested resource was not found.");
+                    }
+                    Log.Error($"Something Went Wrong in the {contextFeature.Error}");
+                    await context.Response.WriteAsync(new ErrorModel
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = message.ToString()
+                    }.ToString());
+                });
+            });
+        }
+    }
+}
