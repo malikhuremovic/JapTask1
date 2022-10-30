5f48266b1b210d2f0804ed0636786114cde6843f
Malik Huremovic <malikhu@maestralsolutions.com>
2022-10-30 20:57:40 +0100

    Student item start and end date calculation method moved from service to util project

--- Back-end/JAPManagementSystem/JAPManagement.Utils/Util/DateCalculator.cs
+++ Back-end/JAPManagementSystem/JAPManagement.Utils/Util/DateCalculator.cs
@@ -1,8 +1,34 @@
-﻿namespace JAPManagenent.Utils.Util
+﻿using JAPManagement.Core.DTOs.StudentDTOs;
+using JAPManagement.Core.Models.StudentModel;
+
+namespace JAPManagenent.Utils.Util
 {
     public class DateCalculator : IDateCalculator
     {
         public static int workingHoursPerDay = 8;
+        public List<AddStudentItemDto> CalculateStartAndEndDate(Student student, List<AddStudentItemDto> studentItemList)
+        {
+            List<AddStudentItemDto> updatedItemList = new List<AddStudentItemDto>();
+            for (int i = 0; i < studentItemList.Count; i++)
+            {
+                var studentItem = studentItemList.ElementAt(i);
+                DateTime previousEndDate;
+                if (i == 0)
+                {
+                    previousEndDate = student.Selection.DateStart;
+                }
+                else
+                {
+                    previousEndDate = studentItemList.ElementAt(i - 1).EndDate;
+                }
+                CalculateTimeDifferenceWithWorkingHours(previousEndDate, studentItem.ExpectedHours, out DateTime newStartDate, out DateTime newEndDate);
+                studentItem.StartDate = newStartDate;
+                studentItem.EndDate = newEndDate;
+                updatedItemList.Add(studentItem);
+            }
+            return updatedItemList;
+        }
+
         public void CalculateTimeDifference(DateTime previousEndDate, int expectedHoursToComplete, out DateTime newStartDate, out DateTime newEndDate)
         {
             newStartDate = previousEndDate;
--- Back-end/JAPManagementSystem/JAPManagement.Utils/Util/IDateCalculator.cs
+++ Back-end/JAPManagementSystem/JAPManagement.Utils/Util/IDateCalculator.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JAPManagement.Core.DTOs.StudentDTOs;
+using JAPManagement.Core.Models.StudentModel;
 
 namespace JAPManagenent.Utils.Util
 {
     public interface IDateCalculator
     {
+        List<AddStudentItemDto> CalculateStartAndEndDate(Student student, List<AddStudentItemDto> studentItemList);
         void CalculateTimeDifference(DateTime previousEndDate, int expectedHoursToComplete, out DateTime newStartDate, out DateTime newEndDate);
         void CalculateTimeDifferenceWithWorkingHours(DateTime previousEndDate, int expectedHoursToComplete, out DateTime newStartDate, out DateTime newEndDate);
 
